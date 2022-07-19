a9d8135b4b4baf5744e18ba0c1d7ae463d65ab04
tejasagashe <109517915+tejasagashe@users.noreply.github.com>
2022-07-19 17:11:30 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+
+
+
+public class main{
+public static void main(String[] args) {
+
+    TicTacToe TTT=new TicTacToe(); // object to call the class
+
+ }
+}
